4c4c3c9b37593f80e8c8db7eaba73c66c3dcf435
sudo <rootkitlove@proton.me>
2025-06-18 20:53:10 -0300

    Atualizar o anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string= Test Kernel by OldCatz
+kernel.string= Kyojuro Kernel by OldCatz
 do.devicecheck=1
 do.modules=0
 do.systemless=1
